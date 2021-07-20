6a3fe280b30b8b1fc0438b76a0304dffd8bc24f6
Notapsychopath <66863481+Notapsychopath@users.noreply.github.com>
2021-07-20 12:34:54 +0530

    Update homePage.js

--- js/homePage.js
+++ js/homePage.js
@@ -1,2 +1,18 @@
 // getting everything from the html
-const postList = document.querySelector(".post-list");
+const menuItems = document.querySelectorAll(".list-item");
+const displayedHead = document.querySelector(".content-head");
+const displayedContent = document.querySelector(".content-content");
+
+menuItems.forEach((item) => {
+  item.addEventListener("click", (e) => {
+    displayedHead.innerText = e.target.parentElement.children[1].innerText;
+    displayedContent.innerText = e.target.parentElement.children[2].innerText;
+  });
+});
+
+const aboutBtn = document.querySelector(".about-btn");
+const aboutMe = document.querySelector(".about-me");
+
+aboutBtn.addEventListener("click", () => {
+  aboutMe.classList.toggle("open");
+});
