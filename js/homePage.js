ab9d3c118e1c24771063f507fc6e5c5518a11892
Notapsychopath <66863481+Notapsychopath@users.noreply.github.com>
2021-07-20 12:52:07 +0530

    Update homePage.js

--- js/homePage.js
+++ js/homePage.js
@@ -1,6 +1,10 @@
-const menuItems = document.querySelectorAll(".list-item");
+// getting everything from the html
+const menuItems = document.querySelectorAll(".post-item");
 const displayedHead = document.querySelector(".content-head");
 const displayedContent = document.querySelector(".content-content");
+
+displayedHead.innerText = menuItems[0].children[1].innerText;
+displayedContent.innerText = menuItems[0].children[2].innerText;
 
 menuItems.forEach((item) => {
   item.addEventListener("click", (e) => {
